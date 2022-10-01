ccca8b26e5bb17423e8bbd62d623279664d59430
TDillman <tobydillman@gmail.com>
2022-10-01 16:03:42 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,456 @@
+import discord
+import logging
+import logging.handlers
+import bot_secrets
+
+from discord import app_commands, ui
+from blizzardapi import BlizzardApi
+from youtube_api import YouTubeDataAPI
+from dataclasses import dataclass, field
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+logging.getLogger('discord.http').setLevel(logging.INFO)
+
+handler = logging.handlers.RotatingFileHandler(
+    filename='discord.log',
+    encoding='utf-8',
+    maxBytes=32 * 1024 * 1024,  # 32 MiB
+    backupCount=5,  # Rotate through 5 files
+)
+dt_fmt = '%Y-%m-%d %H:%M:%S'
+formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+MY_GUILD = discord.Object(id=bot_secrets.GUILD_ID)  # replace with your guild id
+
+yt = YouTubeDataAPI(bot_secrets.YT_DATA_API)
+api_client = BlizzardApi(bot_secrets.BLIZZARD_CLIENT_ID, bot_secrets.BLIZZARD_SECRET_ID)
+
+
+class MyClient(discord.Client):
+    def __init__(self, *, intents: discord.Intents):
+        super().__init__(intents=intents)
+        # A CommandTree is a special type that holds all the application command
+        # state required to make it work. This is a separate class because it
+        # allows all the extra state to be opt-in.
+        # Whenever you want to work with application commands, your tree is used
+        # to store and work with them.
+        # Note: When using commands.Bot instead of discord.Client, the bot will
+        # maintain its own tree instead.
+        self.tree = app_commands.CommandTree(self)
+
+    # In this basic example, we just synchronize the app commands to one guild.
+    # Instead of specifying a guild to every command, we copy over our global commands instead.
+    # By doing so, we don't have to wait up to an hour until they are shown to the end-user.
+    async def setup_hook(self):
+        # This copies the global commands over to your guild.
+        self.tree.copy_global_to(guild=MY_GUILD)
+        await self.tree.sync(guild=MY_GUILD)
+
+
+intents = discord.Intents.default()
+client = MyClient(intents=intents)
+
+
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Game('with fire. Type /help!'))
+    print(f'Logged in as {client.user} (ID: {client.user.id})')
+    print('------')
+
+
+@client.tree.command()
+async def hello(interaction: discord.Interaction):
+    """Says hello!"""
+    await interaction.response.send_message(f'Hi, {interaction.user.mention}')
+
+
+# The rename decorator allows us to change the display of the parameter on Discord.
+# In this example, even though we use `text_to_send` in the code, the client will use `text` instead.
+# Note that other decorators will still refer to it as `text_to_send` in the code.
+@client.tree.command()
+@app_commands.rename(text_to_send='text')
+@app_commands.describe(text_to_send='Text to send in the current channel')
+async def send(interaction: discord.Interaction, text_to_send: str):
+    """Sends the text into the current channel."""
+    await interaction.response.send_message(text_to_send)
+
+
+# A Context Menu command is an app command that can be run on a member or on a message by
+# accessing a menu within the client, usually via right clicking.
+# It always takes an interaction as its first parameter and a Member or Message as its second parameter.
+
+# This context menu command only works on members
+@client.tree.context_menu(name='Show Join Date')
+async def show_join_date(interaction: discord.Interaction, member: discord.Member):
+    # The format_dt function formats the date time into a human readable representation in the official client
+    await interaction.response.send_message(f'{member} joined at {discord.utils.format_dt(member.joined_at)}')
+
+
+@client.tree.command()
+async def help(interaction: discord.Interaction):
+    """List out the bot commands"""
+    embed = discord.Embed(title='Bot Commands')
+    embed.add_field(name='User Commands',
+                    value=f'/abbs\n'
+                    '/aster\n'
+                    '/candercane\n'
+                    '/cool\n'
+                    '/daddychill\n'
+                    '/drew\n'
+                    '/golfclap\n'
+                    '/guacdrop\n'
+                    '/hakkd\n'
+                    '/happybirthday\n'
+                    '/hydrate\n'
+                    '/imdumb\n'
+                    '/imout\n'
+                    '/listen\n'
+                    '/lynkz\n'
+                    '/magic\n'
+                    '/myst\n'
+                    '/nyrixx\n'
+                    '/pig\n'
+                    '/pirate\n'
+                    '/rain\n'
+                    '/rightright\n'
+                    '/risn\n'
+                    '/shit\n'
+                    '/spooky\n'
+                    '/spoon\n'
+                    '/suckit\n'
+                    '/thisisfine\n'
+                    '/wat\n'
+                    '/wtf\n'
+                    '/yzu',
+                    inline=True)
+    embed.add_field(name='Other Commands',
+                    value='/status -- Arygos server status\n'
+                          '/token -- Get the current WoW token value\n'
+                          '/jams -- Search YouTube for a video\n',
+                    inline=True)
+    await interaction.response.send_message(embed=embed, ephemeral=True)
+
+
+@client.tree.command()
+@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+async def spooky(interaction: discord.Interaction):
+    """There's always money in the banana stand!"""
+    await interaction.response.send_message('https://tenor.com/view/arrested-development-claw-hand-juice-box-laughing-evil-laugh-gif-5335530')
+
+
+@client.tree.command()
+@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+async def scrumpy(interaction: discord.Interaction):
+    """Just his opinion"""
+    await interaction.response.send_message('Thinks your bags are awful')
+
+
+@client.tree.command()
+@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+async def golfclap(interaction: discord.Interaction):
+    """Well played"""
+    await interaction.response.send_message('https://tenor.com/view/charlie-sheen-emilio-estevez-golf-clap-men-at-work-gif-7577611')
+
+
+@client.tree.command()
+@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+async def whatever(interaction: discord.Interaction):
+    """Whatever man"""
+    await interaction.response.send_message('https://media.discordapp.net/attachments/765619338337058827/802299499908563024/whatever.gif')
+
+
+@client.tree.command()
+@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+async def cool(interaction: discord.Interaction):
+    """Peralta says"""
+    await interaction.response.send_message('https://tenor.com/view/andy-samberg-brooklyn99-jake-peralta-cool-gif-12063970')
+
+
+@client.tree.command()
+@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+async def myst(interaction: discord.Interaction):
+    """Is it though?"""
+    await interaction.response.send_message('https://tenor.com/view/is-it-though-thor-smile-gif-13334930')
+
+
+@client.tree.command()
+@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+async def beylock(interaction: discord.Interaction):
+    """I love this song"""
+    await interaction.response.send_message('https://imgur.com/a/xux2u6p')
+
+
+@client.tree.command()
+@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+async def happybirthday(interaction: discord.Interaction):
+    """Party at Kat's place!"""
+    await interaction.response.send_message('https://giphy.com/gifs/i8htPQwChFOVcpnImq')
+
+
+@client.tree.command()
+@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+async def magic(interaction: discord.Interaction):
+    """Don't ask how"""
+    await interaction.response.send_message('https://media.discordapp.net/attachments/676183284123828236/761438362720272394/Kat_Confetti.gif')
+
+
+@client.tree.command()
+@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+async def lynkz(interaction: discord.Interaction):
+    """Who is that?"""
+    await interaction.response.send_message('https://media.discordapp.net/attachments/676183284123828236/899091363046522910/unknown.png')
+
+
+@client.tree.command()
+@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+async def candercane(interaction: discord.Interaction):
+    """This child is furious"""
+    await interaction.response.send_message('https://giphy.com/gifs/angry-mad-anger-l1J9u3TZfpmeDLkD6')
+
+
+@client.tree.command()
+@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+async def wat(interaction: discord.Interaction):
+    """wat"""
+    await interaction.response.send_message('https://imgur.com/a/PnB5eFk')
+
+
+@client.tree.command()
+@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+async def thisisfine(interaction: discord.Interaction):
+    """I'll probably survive"""
+    await interaction.response.send_message('https://imgur.com/a/uDAO5In')
+
+
+@client.tree.command()
+@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+async def pirate(interaction: discord.Interaction):
+    """Pirate shimmy!"""
+    await interaction.response.send_message('https://imgur.com/a/TDot4Ba')
+
+
+@client.tree.command()
+@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+async def suckit(interaction: discord.Interaction):
+    """Suck it!"""
+    await interaction.response.send_message('https://imgur.com/Fy6RhWI')
+
+
+@client.tree.command()
+@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+async def risn(interaction: discord.Interaction):
+    """K"""
+    await interaction.response.send_message('https://www.circlek.com/themes/custom/circlek/images/logos/logo-full-color-rgb.jpg')
+
+
+@client.tree.command()
+@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+async def hakkd(interaction: discord.Interaction):
+    """Don't let it happen again"""
+    await interaction.response.send_message('https://tenor.com/view/mad-monster-dont-let-it-happen-again-gif-14024298')
+
+
+@client.tree.command()
+@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+async def wtf(interaction: discord.Interaction):
+    """What the fuck?"""
+    await interaction.response.send_message('https://giphy.com/gifs/what-the-fuck-wtf-ukGm72ZLZvYfS')
+
+
+@client.tree.command()
+@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+async def rain(interaction: discord.Interaction):
+    """Lil bih"""
+    await interaction.response.send_message('https://cdn.discordapp.com/attachments/676183384061378571/856642945481310228/unknown.png')
+
+
+@client.tree.command()
+@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+async def imout(interaction: discord.Interaction):
+    """Peace out"""
+    await interaction.response.send_message('https://media.discordapp.net/attachments/676183306924064768/866005404839837706/sylvanas.gif')
+
+
+@client.tree.command()
+@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+async def daddychill(interaction: discord.Interaction):
+    """What the hell is even that?!"""
+    await interaction.response.send_message('https://tenor.com/view/what-the-hell-is-even-gif-20535402')
+
+
+@client.tree.command()
+@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+async def guacdrop(interaction: discord.Interaction):
+    """Not the guacamole!"""
+    await interaction.response.send_message('https://media.discordapp.net/attachments/917450971569877044/917465265036488704/20211105_213516.jpg')
+
+
+@client.tree.command()
+@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+async def rightright(interaction: discord.Interaction):
+    """From a show about nothing"""
+    await interaction.response.send_message('https://tenor.com/view/seinfeld-jerry-seinfeld-oh-right-agree-gif-4436696')
+
+
+@client.tree.command()
+@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+async def shit(interaction: discord.Interaction):
+    """Get your poop in a group"""
+    await interaction.response.send_message('https://giphy.com/gifs/get-well-then-woTdBa435yy6A')
+
+
+@client.tree.command()
+@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+async def hydrate(interaction: discord.Interaction):
+    """Hydration is important"""
+    await interaction.response.send_message('https://tenor.com/view/water-smile-drink-water-gif-13518129')
+
+
+@client.tree.command()
+@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+async def spoon(interaction: discord.Interaction):
+    """My spoon is too big"""
+    await interaction.response.send_message('https://media.discordapp.net/attachments/503025662546935809/747820543918735370/A_little_party_never_killed_no_body_gif.gif')
+
+
+@client.tree.command()
+@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+async def imdumb(interaction: discord.Interaction):
+    """I'm dumb"""
+    await interaction.response.send_message('https://tenor.com/view/winston-schmidt-max-greenfield-new-girl-gif-15041554')
+
+
+@client.tree.command()
+@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+async def aster(interaction: discord.Interaction):
+    """That face. That goddamn face."""
+    await interaction.response.send_message('https://cdn.discordapp.com/attachments/938971434246631435/940347663533084732/Chaotic_Aster.png')
+
+
+@client.tree.command()
+@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+async def abba(interaction: discord.Interaction):
+    """Hehehehehehehe"""
+    await interaction.response.send_message('https://tenor.com/view/lizard-laughing-laughinglizard-hehehe-gif-5215392')
+
+
+@client.tree.command()
+@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+async def drew(interaction: discord.Interaction):
+    """Cold. Dead. Lifeless."""
+    await interaction.response.send_message('https://tenor.com/view/sparkly-eyes-joy-happy-anime-hug-gif-15852045')
+
+
+@client.tree.command()
+@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+async def pig(interaction: discord.Interaction):
+    """This little piggy..."""
+    await interaction.response.send_message('https://tenor.com/view/pig-cute-gif-21946909')
+
+
+@client.tree.command()
+@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+async def listen(interaction: discord.Interaction):
+    """listen here"""
+    await interaction.response.send_message('https://i.pinimg.com/originals/ef/a6/48/efa648c67f3cb05287ded99612af130f.png')
+
+
+@client.tree.command()
+@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+async def nyrixx(interaction: discord.Interaction):
+    """Never sneak up on a Schrute."""
+    await interaction.response.send_message('https://tenor.com/view/office-dwight-schrute-surprised-gif-14541388')
+
+
+@client.tree.command()
+@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+async def yzu(interaction: discord.Interaction):
+    """eli5"""
+    await interaction.response.send_message('https://tenor.com/view/confused-the-office-michael-scott-steve-carell-explain-this-to-me-like-im-five-gif-4527435')
+
+
+@client.tree.command()
+@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+@app_commands.describe(search='Search criteria')
+async def jams(interaction: discord.Interaction, search: str):
+    """Search YouTube for a video!"""
+    results = yt.search(search, max_results=5, order='relevance')
+    if len(results) == 0:
+        await interaction.response.send_message(f"No results for {search}")
+    else:
+        youtube_video_url = f"https://www.youtube.com/watch?v={results[0]['video_id']}"
+        await interaction.response.send_message(youtube_video_url)
+
+
+@client.tree.command()
+@app_commands.describe(mock='Mocking text')
+async def mock(interaction: discord.Interaction, mock: str):
+    """QuIt MaKiNg FuN oF mE"""
+    mocking_text = (''.join([letter.lower() if index % 2 == 0 else letter.upper() for index, letter in enumerate(mock)]))
+    await interaction.response.send_message(mocking_text)
+
+
+@client.tree.command()
+@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+async def token(interaction: discord.Interaction):
+    """
+    Returns an embed with the current WoW token price in gold
+    """
+    token_object = api_client.wow.game_data.get_token_index("us", "en_US")
+
+    embed = discord.Embed(title='WoW Token')
+    embed.add_field(name='Current Price', value=f'{token_object["price"] / 10000:,.0f} gold')
+    embed.set_thumbnail(
+        url='https://cdn.discordapp.com/attachments/676183284123828236/679823287521771602/mightcolored'
+            'finishedsmall.png')
+    await interaction.response.send_message(embed=embed)
+
+
+@client.tree.command()
+@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+async def status(interaction: discord.Interaction):
+    """Status of the Arygos server"""
+
+    server_object = api_client.wow.game_data.get_connected_realm("us", "en_US", 99)
+
+    @dataclass
+    class Server:
+        status: str = server_object['status']['name']
+        population: str = server_object['population']['name']
+        has_queue: bool = server_object['has_queue']
+        region: str = server_object['realms'][0]['region']['name']
+        country: str = server_object['realms'][0]['category']
+        timezone: str = server_object['realms'][0]['timezone']
+        connections: list = field(default_factory=lambda: [server_object['realms'][x]['name'] for x in
+                                                           range(len(server_object['realms']))])
+        name: str = 'Arygos'
+
+    server = Server()
+
+    if server.status == "Up":
+        status_color = 0x00ff00  # Green for up
+    else:
+        status_color = 0xff0000  # Red for down
+
+    server_string = ', '.join(str(name) for name in server.connections)
+
+    embed = discord.Embed(title='Arygos', color=status_color)
+    embed.add_field(name='Current Status', value=f'Server is currently {server.status}', inline=True)
+    if server.population == 'Offline':
+        embed.add_field(name='Current Population', value=f'This server is currently {server.population}')
+    else:
+        embed.add_field(name='Current Population', value=f'This is a {server.population} pop server', inline=True)
+    embed.add_field(name='Connected Realms', value=server_string, inline=False)
+    if server.has_queue:
+        embed.add_field(name='Queue Active', value='Server has a login queue')
+    embed.add_field(name="Timezone", value=server.timezone, inline=True)
+    embed.set_thumbnail(
+        url='https://cdn.discordapp.com/attachments/676183284123828236/679823287521771602/mightcolored'
+            'finishedsmall.png')
+    await interaction.response.send_message(embed=embed)
+
+
+client.run('Nzc5NTAyNzYwMjE4NzIyMzQ2.GecYDc.IIu4pU0a6u8KUnq7EpU8LhLK53TYUbVTLfLTQA', log_handler=None)
