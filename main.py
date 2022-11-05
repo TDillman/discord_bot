afcb6b82688a202701f5071d37f16bddb868aebf
TDillman <tobydillman@gmail.com>
2022-11-05 16:56:09 -0500

    Added in local images for spec icons

--- main.py
+++ main.py
@@ -46,68 +46,68 @@ kat_gif_list = [
 
 wow_spec_dict = {
     "Death Knight" : {
-        "a Blood" : [0xC41E3A, 'Tank', 'A dark guardian who manipulates and corrupts life energy to sustain <him/her>self in the face of an enemy onslaught.', 'https://static.wikia.nocookie.net/wowpedia/images/4/45/Spell_deathknight_bloodpresence.png'],
-        "a Frost" : [0xC41E3A, 'Melee DPS', 'An icy harbinger of doom, channeling runic power and delivering vicious weapon strikes.', 'https://static.wikia.nocookie.net/wowpedia/images/3/3e/Spell_deathknight_frostpresence.png'],
-        "an Unholy" : [0xC41E3A, 'Melee DPS', 'A master of death and decay, spreading infection and controlling undead minions to do <his/her> bidding.', 'https://static.wikia.nocookie.net/wowpedia/images/6/6f/Spell_deathknight_unholypresence.png']
+        "a Blood" : [0xC41E3A, 'Tank', 'A dark guardian who manipulates and corrupts life energy to sustain <him/her>self in the face of an enemy onslaught.', 'Spell_deathknight_bloodpresence.png'],
+        "a Frost" : [0xC41E3A, 'Melee DPS', 'An icy harbinger of doom, channeling runic power and delivering vicious weapon strikes.', 'Spell_deathknight_frostpresence.png'],
+        "an Unholy" : [0xC41E3A, 'Melee DPS', 'A master of death and decay, spreading infection and controlling undead minions to do <his/her> bidding.', 'Spell_deathknight_unholypresence.png']
     },
     "Demon Hunter" : {
-        "a Vengeance": [0xA330C9, 'Tank', 'Embraces the demon within to incinerate enemies and protect their allies.', 'https://static.wikia.nocookie.net/wowpedia/images/2/25/Demon_hunter_vengeance_icon.png'],
-        "a Havoc" : [0xA330C9, 'Melee DPS', 'A brooding master of warglaives and the destructive power of fel magic.', 'https://static.wikia.nocookie.net/wowpedia/images/2/2d/Demon_hunter_havoc_icon.png']
+        "a Vengeance": [0xA330C9, 'Tank', 'Embraces the demon within to incinerate enemies and protect their allies.', 'Demon_hunter_vengeance_icon.png'],
+        "a Havoc" : [0xA330C9, 'Melee DPS', 'A brooding master of warglaives and the destructive power of fel magic.', 'Demon_hunter_havoc_icon.png']
     },
     "Druid" : {
-        "a Guardian" : [0xFF7C0A, 'Tank', 'Takes on the form of a mighty bear to absorb damage and protect allies.', 'https://static.wikia.nocookie.net/wowpedia/images/b/bb/Ability_racial_bearform.png'],
-        "a Feral" : [0xFF7C0A, 'Melee DPS', 'Takes on the form of a great cat to deal damage with bleeds and bites.', 'https://static.wikia.nocookie.net/wowpedia/images/4/48/Ability_druid_catform.png'],
-        "a Balance" : [0xFF7C0A, 'Ranged DPS', 'Can take on the form of a powerful Moonkin, balancing the power of Arcane and Nature magic to destroy enemies at a distance.', 'https://static.wikia.nocookie.net/wowpedia/images/1/14/Spell_nature_starfall.png'],
-        "a Restoration" : [0xFF7C0A, 'Healer', 'Uses heal-over-time Nature spells to keep allies alive.', 'https://static.wikia.nocookie.net/wowpedia/images/3/37/Spell_nature_healingtouch.png']
+        "a Guardian" : [0xFF7C0A, 'Tank', 'Takes on the form of a mighty bear to absorb damage and protect allies.', 'Ability_racial_bearform.png'],
+        "a Feral" : [0xFF7C0A, 'Melee DPS', 'Takes on the form of a great cat to deal damage with bleeds and bites.', 'Ability_druid_catform.png'],
+        "a Balance" : [0xFF7C0A, 'Ranged DPS', 'Can take on the form of a powerful Moonkin, balancing the power of Arcane and Nature magic to destroy enemies at a distance.', 'Spell_nature_starfall.png'],
+        "a Restoration" : [0xFF7C0A, 'Healer', 'Uses heal-over-time Nature spells to keep allies alive.', 'Spell_nature_healingtouch.png']
     },
     "Evoker" : {
         "a Devastation" : [0x33937F, 'Ranged DPS', 'A ranged DPS specialization that mainly focuses on the magic of the red and blue dragonflights to harm foes from a distance with a variety of concentrated spells and powerful breath attacks.', 'https://upload.wikimedia.org/wikipedia/en/5/53/Spyro.png'],
         "a Preservation" : [0x33937F, 'Healer', 'A healer specialization that largely utilizes the magic of the green and bronze dragonflights to heal allies with breaths and even rewind time on wounds themselves!', 'https://upload.wikimedia.org/wikipedia/en/5/53/Spyro.png'],
     },
     "Hunter" : {
-        "a Beast Mastery" : [0xAAD372, 'Ranged DPS', 'A master of the wild who can tame a wide variety of beasts to assist <him/her> in combat.', 'https://static.wikia.nocookie.net/wowpedia/images/9/98/Ability_hunter_bestialdiscipline.png'],
-        "a Marksmanship" : [0xAAD372, 'Ranged DPS', 'A master archer or sharpshooter who excels in bringing down enemies from afar.', 'https://static.wikia.nocookie.net/wowpedia/images/d/de/Ability_hunter_focusedaim.png'],
-        "a Survival" : [0xAAD372, 'Melee DPS', 'A rugged tracker who favors using animal venom, explosives and traps as deadly weapons.', 'https://static.wikia.nocookie.net/wowpedia/images/d/da/Ability_hunter_camouflage.png']
+        "a Beast Mastery" : [0xAAD372, 'Ranged DPS', 'A master of the wild who can tame a wide variety of beasts to assist <him/her> in combat.', 'Ability_hunter_bestialdiscipline.png'],
+        "a Marksmanship" : [0xAAD372, 'Ranged DPS', 'A master archer or sharpshooter who excels in bringing down enemies from afar.', 'Ability_hunter_focusedaim.png'],
+        "a Survival" : [0xAAD372, 'Melee DPS', 'A rugged tracker who favors using animal venom, explosives and traps as deadly weapons.', 'Ability_hunter_camouflage.png']
     },
     "Mage" : {
-        "an Arcane" : [0x3FC7EB, 'Ranged DPS', 'Manipulates the arcane, destroying enemies with overwhelming power.', 'https://static.wikia.nocookie.net/wowpedia/images/d/d2/Spell_holy_magicalsentry.png'],
-        "a Fire" : [0x3FC7EB, 'Ranged DPS', 'Ignites enemies with balls of fire and combustive flames.', 'https://static.wikia.nocookie.net/wowpedia/images/4/45/Spell_fire_firebolt02.png'],
-        "a Frost" : [0x3FC7EB, 'Ranged DPS', 'Freezes enemies in their tracks and shatters them with Frost magic.', 'https://static.wikia.nocookie.net/wowpedia/images/1/1e/Spell_frost_frostbolt02.png']
+        "an Arcane" : [0x3FC7EB, 'Ranged DPS', 'Manipulates the arcane, destroying enemies with overwhelming power.', 'Spell_holy_magicalsentry.png'],
+        "a Fire" : [0x3FC7EB, 'Ranged DPS', 'Ignites enemies with balls of fire and combustive flames.', 'Spell_fire_firebolt02.png'],
+        "a Frost" : [0x3FC7EB, 'Ranged DPS', 'Freezes enemies in their tracks and shatters them with Frost magic.', 'Spell_frost_frostbolt02.png']
     },
     "Monk" : {
-        "a Brewmaster" : [0x00FF98, 'Tank', 'A sturdy brawler who uses liquid fortification and unpredictable movement to avoid damage and protect allies.', 'https://static.wikia.nocookie.net/wowpedia/images/d/d1/Spell_monk_brewmaster_spec.png'],
-        "a Windwalker" : [0x00FF98, 'Melee DPS', 'A martial artist without peer who pummels foes with hands and fists.', 'https://static.wikia.nocookie.net/wowpedia/images/a/aa/Spell_monk_windwalker_spec.png'],
-        "a Mistweaver" : [0x00FF98, 'Healer', 'A healer who mixes traditional herbal medicine with Pandaren martial arts.', 'https://static.wikia.nocookie.net/wowpedia/images/4/4e/Spell_monk_mistweaver_spec.png']
+        "a Brewmaster" : [0x00FF98, 'Tank', 'A sturdy brawler who uses liquid fortification and unpredictable movement to avoid damage and protect allies.', 'Spell_monk_brewmaster_spec.png'],
+        "a Windwalker" : [0x00FF98, 'Melee DPS', 'A martial artist without peer who pummels foes with hands and fists.', 'Spell_monk_windwalker_spec.png'],
+        "a Mistweaver" : [0x00FF98, 'Healer', 'A healer who mixes traditional herbal medicine with Pandaren martial arts.', 'Spell_monk_mistweaver_spec.png']
     },
     "Paladin" : {
-        "a Protection" : [0xF48CBA, 'Tank', 'Uses Holy magic to shield <himself/herself> and defend allies from attackers.', 'https://static.wikia.nocookie.net/wowpedia/images/2/2f/Ability_paladin_shieldofthetemplar.png'],
-        "a Holy" : [0xF48CBA, 'Healer', 'Invokes the power of the Light to protect and to heal.', 'https://static.wikia.nocookie.net/wowpedia/images/b/b4/Spell_holy_holybolt.png'],
-        "a Retribution" : [0xF48CBA, 'Melee DPS', 'A righteous crusader who judges and punishes opponents with weapons and Holy magic.', 'https://static.wikia.nocookie.net/wowpedia/images/0/0a/Spell_holy_auraoflight.png']
+        "a Protection" : [0xF48CBA, 'Tank', 'Uses Holy magic to shield <himself/herself> and defend allies from attackers.', 'Ability_paladin_shieldofthetemplar.png'],
+        "a Holy" : [0xF48CBA, 'Healer', 'Invokes the power of the Light to protect and to heal.', 'Spell_holy_holybolt.png'],
+        "a Retribution" : [0xF48CBA, 'Melee DPS', 'A righteous crusader who judges and punishes opponents with weapons and Holy magic.', 'Spell_holy_auraoflight.png']
     },
     "Priest" : {
-        "a Shadow" : [0xFFFFFF, 'Ranged DPS', 'Uses sinister Shadow magic, especially damage-over-time spells, to eradicate enemies.', 'https://static.wikia.nocookie.net/wowpedia/images/3/30/Spell_shadow_shadowwordpain.png'],
-        "a Discipline" : [0xFFFFFF, 'Healer', 'Uses magic to shield allies from taking damage as well as heal their wounds.', 'https://static.wikia.nocookie.net/wowpedia/images/3/35/Spell_holy_powerwordshield.png/revision'],
-        "a Holy" : [0xFFFFFF, 'Healer', 'A versatile healer who can reverse damage on individuals or groups and even heal from beyond the grave.', 'https://static.wikia.nocookie.net/wowpedia/images/d/d6/Spell_holy_guardianspirit.png']
+        "a Shadow" : [0xFFFFFF, 'Ranged DPS', 'Uses sinister Shadow magic, especially damage-over-time spells, to eradicate enemies.', 'Spell_shadow_shadowwordpain.png'],
+        "a Discipline" : [0xFFFFFF, 'Healer', 'Uses magic to shield allies from taking damage as well as heal their wounds.', 'Spell_holy_powerwordshield.png'],
+        "a Holy" : [0xFFFFFF, 'Healer', 'A versatile healer who can reverse damage on individuals or groups and even heal from beyond the grave.', 'Spell_holy_guardianspirit.png']
     },
     "Rogue" : {
-        "an Assassination" : [0xFFF468, 'Melee DPS', 'A deadly master of poisons who dispatches victims with vicious dagger strikes.', 'https://static.wikia.nocookie.net/wowpedia/images/6/65/Ability_rogue_eviscerate.png'],
-        "an Outlaw" : [0xFFF468, 'Melee DPS', 'A swashbuckler who uses agility and guile to stand toe-to-toe with enemies.', 'https://static.wikia.nocookie.net/wowpedia/images/d/df/Inv_sword_30.png'],
-        "a Subtlety" : [0xFFF468, 'Melee DPS', 'A dark stalker who leaps from the shadows to ambush <his/her> unsuspecting prey.', 'https://static.wikia.nocookie.net/wowpedia/images/f/f8/Ability_stealth.png']
+        "an Assassination" : [0xFFF468, 'Melee DPS', 'A deadly master of poisons who dispatches victims with vicious dagger strikes.', 'Ability_rogue_eviscerate.png'],
+        "an Outlaw" : [0xFFF468, 'Melee DPS', 'A swashbuckler who uses agility and guile to stand toe-to-toe with enemies.', 'Inv_sword_30.png'],
+        "a Subtlety" : [0xFFF468, 'Melee DPS', 'A dark stalker who leaps from the shadows to ambush <his/her> unsuspecting prey.', 'Ability_stealth.png']
     },
     "Shaman" : {
-        "an Elemental" : [0x0070DD, 'Ranged DPS', 'A spellcaster who harnesses the destructive forces of nature and the elements.', 'https://static.wikia.nocookie.net/wowpedia/images/4/4c/Spell_nature_lightning.png'],
-        "an Enhancement" : [0x0070DD, 'Melee DPS', 'A totemic warrior who strikes foes with weapons imbued with elemental power.', 'https://static.wikia.nocookie.net/wowpedia/images/c/c5/Spell_shaman_improvedstormstrike.png'],
-        "a Restoration" : [0x0070DD, 'Healer', 'A healer who calls upon ancestral spirits and the cleansing power of water to mend allies\' wounds.', 'https://static.wikia.nocookie.net/wowpedia/images/d/da/Spell_nature_magicimmunity.png']
+        "an Elemental" : [0x0070DD, 'Ranged DPS', 'A spellcaster who harnesses the destructive forces of nature and the elements.', 'Spell_nature_lightning.png'],
+        "an Enhancement" : [0x0070DD, 'Melee DPS', 'A totemic warrior who strikes foes with weapons imbued with elemental power.', 'Spell_shaman_improvedstormstrike.png'],
+        "a Restoration" : [0x0070DD, 'Healer', 'A healer who calls upon ancestral spirits and the cleansing power of water to mend allies\' wounds.', 'Spell_nature_magicimmunity.png']
     },
     "Warlock" : {
-        "an Affliction" : [0x8788EE, 'Ranged DPS', 'A master of Shadow magic who specializes in drains and damage-over-time spells.', 'https://static.wikia.nocookie.net/wowpedia/images/e/ef/Spell_shadow_deathcoil.png'],
-        "a Demonology" : [0x8788EE, 'Ranged DPS', 'A master of demonic magic who transforms into a demon and compels demonic powers to aid him.', 'https://static.wikia.nocookie.net/wowpedia/images/d/dd/Spell_shadow_metamorphosis.png'],
-        "a Destruction" : [0x8788EE, 'Ranged DPS', 'A master of chaos who calls down fire to burn and demolish enemies.', 'https://static.wikia.nocookie.net/wowpedia/images/f/fc/Spell_shadow_rainoffire.png']
+        "an Affliction" : [0x8788EE, 'Ranged DPS', 'A master of Shadow magic who specializes in drains and damage-over-time spells.', 'Spell_shadow_deathcoil.png'],
+        "a Demonology" : [0x8788EE, 'Ranged DPS', 'A master of demonic magic who transforms into a demon and compels demonic powers to aid him.', 'Spell_shadow_metamorphosis.png'],
+        "a Destruction" : [0x8788EE, 'Ranged DPS', 'A master of chaos who calls down fire to burn and demolish enemies.', 'Spell_shadow_rainoffire.png']
     },
     "Warrior" : {
-        "a Protection" : [0xC69B6D, 'Tank', 'A stalwart protector who uses a shield to safeguard <himself/herself> and <his/her> allies.', 'https://static.wikia.nocookie.net/wowpedia/images/b/b1/Ability_warrior_defensivestance.png'],
-        "an Arms" : [0xC69B6D, 'Melee DPS', 'A battle-hardened master of two-handed weapons, using mobility and overpowering attacks to strike <his/her> opponents down.', 'https://static.wikia.nocookie.net/wowpedia/images/e/e7/Ability_warrior_savageblow.png'],
-        "a Fury" : [0xC69B6D, 'Melee DPS', 'A furious berserker wielding a weapon in each hand, unleashing a flurry of attacks to carve <his/her> opponents to pieces.', 'https://static.wikia.nocookie.net/wowpedia/images/2/24/Ability_warrior_innerrage.png']
+        "a Protection" : [0xC69B6D, 'Tank', 'A stalwart protector who uses a shield to safeguard <himself/herself> and <his/her> allies.', 'Ability_warrior_defensivestance.png'],
+        "an Arms" : [0xC69B6D, 'Melee DPS', 'A battle-hardened master of two-handed weapons, using mobility and overpowering attacks to strike <his/her> opponents down.', 'Ability_warrior_savageblow.png'],
+        "a Fury" : [0xC69B6D, 'Melee DPS', 'A furious berserker wielding a weapon in each hand, unleashing a flurry of attacks to carve <his/her> opponents to pieces.', 'Ability_warrior_innerrage.png']
     }
 }
 
@@ -554,14 +554,15 @@ async def pick(interaction: discord.Interaction):
     wow_spec = random.choice(list(wow_spec_dict[wow_class].keys()))
 
     color = wow_spec_dict[wow_class][wow_spec][0]
-    type = wow_spec_dict[wow_class][wow_spec][1]
+    spec_type = wow_spec_dict[wow_class][wow_spec][1]
     description = wow_spec_dict[wow_class][wow_spec][2]
     icon = wow_spec_dict[wow_class][wow_spec][3]
 
-    embed = discord.Embed(title=f"You should play {wow_spec} {wow_class} ({type})", color=color, description=description)
-    embed.set_thumbnail(url=icon)
+    embed = discord.Embed(title=f"You should play {wow_spec} {wow_class} ({spec_type})", color=color, description=description)
+    file = discord.File(f'./wow_icons/{icon}', filename=icon)
+    embed.set_thumbnail(url=f'attachment://{icon}')
 
-    await interaction.response.send_message(embed=embed)
+    await interaction.response.send_message(file=file, embed=embed)
 
 
 movie_worksheet = gc.open('Beyplex').get_worksheet(0)
